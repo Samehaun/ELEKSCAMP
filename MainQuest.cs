39b6f527ccfabdb45cd2f6f99820c9f5290c86cd
Samehaun <samehaun@gmail.com>
2021-09-22 19:09:50 +0300

    Initializing player

---
+++ MainQuest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELEKSUNI
+{
+    class MainQuest
+    {
+        private string name;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            Player player = new Player(Console.ReadLine());
+            Console.WriteLine(player.name);
+            Console.WriteLine(player.health);
+            Console.WriteLine(player.coin);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELEKSUNI
+{
+    class Player
+    {
+        public string name { get; set; }
+        public int coin { get; set; }
+        public int health { get; set; }
+        public Player(string playerName)
+        {
+            name = playerName;
+            coin = 0;
+            health = 100;
+        }
+    }
+}
